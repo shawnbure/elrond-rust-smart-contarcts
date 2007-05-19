9d78310ef94156d44eeda9eece2674e009c72bd6
Newname <new@email>
2007-05-19 01:01:01 -0700

    added second devnet snipet file

---
+++ nft_template/interaction/devnet_chub.sh
@@ -0,0 +1,78 @@
+#Fill this with the good values
+MY_WALLET_PEM="~/Wallets/WalletKey.pem"
+PROXY="https://devnet-gateway.elrond.com"
+CHAIN_ID="D"
+MY_ADDRESS="erd17s2pz8qrds6ake3qwheezgy48wzf7dr5nhdpuu2h4rr4mt5rt9ussj7xzh"
+
+MY_TOKEN_NAME="0x43485542"
+MY_TOKEN_TICKER="0x43485542"
+
+# This is how you get your token ID
+issueNft() {
+    erdpy --verbose contract call erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u \
+        --pem=${MY_WALLET_PEM} \
+        --recall-nonce \
+        --value 50000000000000000 \
+        --gas-limit 60000000 \
+        --function "issueNonFungible" \
+        --arguments ${MY_TOKEN_NAME} ${MY_TOKEN_TICKER} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --send || return
+}
+
+WASM="../output/nft_template.wasm"
+MY_TOKEN_ID="0x434855422d646334393036" #Fill this after issue
+ROYALTIES=0x02EE #7.5%
+MY_TOKEN_NAME_BASE="0x4368756262696672656e73"
+MY_TOKEN_IMAGE_BASE_URI="0x68747470733a2f2f7777772e63687562626976657273652e636f6d2f6672656e"
+MY_TOKEN_METADATA_BASE_URI="0x68747470733a2f2f7777772e63687562626976657273652e636f6d2f6170692f6d6574612f31"
+PRICE=1000000000000000000 #1EGLD
+MAX_SUPPLY=10000
+SALE_START=0
+
+deploy() {
+    erdpy --verbose contract deploy --recall-nonce \
+        --bytecode=${WASM} \
+        --pem=${MY_WALLET_PEM} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=100000000 \
+        --arguments ${MY_TOKEN_ID} ${ROYALTIES} ${MY_TOKEN_NAME_BASE} ${MY_TOKEN_IMAGE_BASE_URI} ${MY_TOKEN_METADATA_BASE_URI} ${PRICE} ${MAX_SUPPLY} ${SALE_START} \
+        --send || return
+}
+
+CONTRACT_ADDRESS="erd1qqqqqqqqqqqqqpgqlmjcxwj4wcke2sc0vtz5lgdmfyq9krr6t9ush8htdw"
+CONTRACT_ADDRESS_HEX="0x00000000000000000500fee5833a55762d95430f62c54fa1bb49005b0c7a5979"
+
+upgrade() {
+    erdpy --verbose contract upgrade ${CONTRACT_ADDRESS} --recall-nonce \
+        --bytecode=${WASM} \
+        --pem=${MY_WALLET_PEM} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --gas-limit=100000000 \
+        --arguments ${MY_TOKEN_ID} ${ROYALTIES} ${MY_TOKEN_NAME_BASE} ${MY_TOKEN_IMAGE_BASE_URI} ${MY_TOKEN_METADATA_BASE_URI} ${PRICE} ${MAX_SUPPLY} ${SALE_START} \
+        --send || return
+}
+
+setSpecialRole() {
+    erdpy --verbose contract call erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u \
+        --pem=${MY_WALLET_PEM} \
+        --recall-nonce \
+        --gas-limit 60000000 \
+        --function "setSpecialRole" \
+        --arguments ${MY_TOKEN_ID} ${CONTRACT_ADDRESS_HEX} 0x45534454526f6c654e4654437265617465 \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --send || return
+}
+
+mintTokens() {
+    erdpy --verbose contract call ${CONTRACT_ADDRESS} \
+        --pem=${MY_WALLET_PEM} \
+        --recall-nonce \
+        --value 1000000000000000000 \
+        --gas-limit 60000000 \
+        --function "mintTokens" \
+        --arguments $1 \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --send || return
+}
+
