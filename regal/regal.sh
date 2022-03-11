4666f396463b641ca8af0b8e7e4a634ad805cecb
“Shawn <shawnbure@outlook.com>
2022-03-11 06:38:41 -0700

    regal script

--- regal/regal.sh
+++ regal/regal.sh
@@ -1,11 +1,12 @@
 ############### DEVELOPMENT ################################
 
-MY_WALLET_PEM="cp-new-wallet.pem"
+MY_WALLET_PEM="../cp-new-wallet.pem"
 PROXY="https://devnet-gateway.elrond.com"
 CHAIN_ID="D"
 TEMPLATE_CONTRACT_ADDRESS="erd1qqqqqqqqqqqqqpgqj5qg3p4uq3nup4mjrmz9c2w24k00fajty4wsh65lq7"
 DAO_WALLET_ADDRESS_BECH32="54d029b2a71cfec4497cc56fecb299534847aee2465cd2d32d9e55c1dc861b96"
-
+TOKEN_IDENTIFIER="SMBSMB-4b24c7"
+RECEIVER_ADDRESS="erd1q347usx9ajuhzxh9343a7s8py5e95n2urme5wpa0whjl5qhgu3ks8lujw9"
 ############### DEVELOPMENT ################################
 
 
@@ -16,11 +17,21 @@ DAO_WALLET_ADDRESS_BECH32="54d029b2a71cfec4497cc56fecb299534847aee2465cd2d32d9e5
 #CHAIN_ID="1"
 #TEMPLATE_CONTRACT_ADDRESS="erd1qqqqqqqqqqqqqpgqjc2aaea3mzr375nqnulya6x4lxl0qrp6y4wslghcz6"
 #DAO_WALLET_ADDRESS_BECH32="0x6b3d87c350a9fc286199e186de9e479dc9a2b58808083b7c419afbf358082319" 
-
+#HEX_TOKEN_IDENTIFIER=""
+#RECEIVER_ADDRESS=""
 ############### PRODUCTION ################################
 
 HEX_PREFIXED_BECH32_ADDRESS="0x"
 HEX_PREFIXED_BECH32_ADDRESS+=${DAO_WALLET_ADDRESS_BECH32}
+
+HEX_TOKEN_IDENTIFIER=$(xxd -pu <<< ${TOKEN_IDENTIFIER})
+HEX_RECEIVER_ADDRESS=$(xxd -pu <<< ${RECEIVER_ADDRESS})
+HEX_ROLE_BURN=$(xxd -pu <<< "ESDTRoleLocalBurn")
+HEX_CAN_BURN=$(xxd -pu <<< "canBurn")
+HEX_TRUE=$(xxd -pu <<< "true")
+HEX_FALSE=$(xxd -pu <<< "false")
+
+echo $HEX_TOKEN_IDENTIFIER
 
 updateBuyerWhitelistCheckOn() {
     erdpy --verbose contract call ${TEMPLATE_CONTRACT_ADDRESS} --recall-nonce \
@@ -95,6 +106,52 @@ setFinalSalePrice() {
         
         getResults "getPrice"
 }
+
+setSpecialBurnRole() {
+    erdpy --verbose tx new --recall-nonce \
+        --pem=${MY_WALLET_PEM} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --value=0 \
+        --receiver ${RECEIVER_ADDRESS} \
+        --data "setSpecialRole@${HEX_TOKEN_IDENTIFIER}@${HEX_RECEIVER_ADDRESS}@${HEX_ROLE_BURN}" \
+        --gas-limit=20000000 \
+        --send || return
+}
+
+setBurnRoleOn() {
+    erdpy --verbose tx new --recall-nonce \
+        --pem=${MY_WALLET_PEM} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --value=0 \
+        --receiver ${RECEIVER_ADDRESS} \
+        --data "controlChanges@${HEX_TOKEN_IDENTIFIER}@${HEX_ROLE_BURN}" \
+        --gas-limit=20000000 \
+        --send || return
+}
+
+setCanBurnOn() {
+    erdpy --verbose tx new --recall-nonce \
+        --pem=${MY_WALLET_PEM} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --value=0 \
+        --receiver ${RECEIVER_ADDRESS} \
+        --data "controlChanges@${HEX_TOKEN_IDENTIFIER}@${HEX_CAN_BURN}@${HEX_TRUE}" \
+        --gas-limit=20000000 \
+        --send || return
+}
+
+setCanBurnOff() {
+    erdpy --verbose tx new --recall-nonce \
+        --pem=${MY_WALLET_PEM} \
+        --proxy=${PROXY} --chain=${CHAIN_ID} \
+        --value=0 \
+        --receiver ${RECEIVER_ADDRESS} \
+        --data "controlChanges@${HEX_TOKEN_IDENTIFIER}@${HEX_CAN_BURN}@${HEX_FALSE}" \
+        --gas-limit=20000000 \
+        --send || return
+}
+
+
 
 getResults() {
 
